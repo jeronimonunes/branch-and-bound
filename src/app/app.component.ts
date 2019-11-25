60da38766ff76cf08e0231814ed092df2c1ac31a
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2019-11-25 11:42:42 -0300

    Loading animation with a graph mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
+import { Network, Options, Data, Edge, DataSet } from 'vis';
+import { edit } from 'ace-builds';
+import { animationFrameScheduler, interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getSpinnerImageOnTime } from './spinner';
+
 
 import './prog-lin.ace.mod';
 import 'ace-builds/src-noconflict/theme-monokai';
-import { edit } from 'ace-builds';
 
 const THEME = 'ace/theme/monokai';
 const MODE = 'ace/mode/progLin';
@@ -12,13 +17,86 @@ const MODE = 'ace/mode/progLin';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
 
   @ViewChild('editor', { static: true }) editor!: ElementRef<HTMLDivElement>;
+  @ViewChild('graphDiv', { static: true }) graphDiv!: ElementRef<HTMLDivElement>;
+
+  network: Network;
+  animationSubscription: Subscription;
 
   ngOnInit() {
     const inputEditor = edit(this.editor.nativeElement);
     inputEditor.setTheme(THEME);
     inputEditor.getSession().setMode(MODE);
+    inputEditor.setValue(`max(-3a -4b +5c -5d)
+    st:
+        +1a +1b +0c +0d <= +5;
+        -1a +0b -5c +5d <= -10;
+        +2a +1b +1c -1d <= +10;
+        -2a -1b -1c +1d <= -10;
+        a >= 0;
+        b >= 0;
+        c >= 0;
+        d >= 0;
+`);
+    inputEditor.clearSelection();
+
+    const options: Options = {
+      physics: true,
+      edges: {
+        smooth: {
+          enabled: true,
+          type: 'cubicBezier',
+          forceDirection: 'horizontal',
+          roundness: .6
+        },
+        arrows: { to: true }
+      },
+      nodes: {
+        image: getSpinnerImageOnTime(0),
+        shape: 'circle',
+        color: {
+          background: 'white'
+        },
+      },
+      layout: {
+        hierarchical: {
+          enabled: true,
+          direction: 'UD',
+          sortMethod: 'directed'
+        }
+      }
+    };
+
+    const nodes = new DataSet([
+      { id: 'program', label: 'PROGRAM', shape: 'circularImage' },
+      { id: 'p-identifier', label: 'test' },
+      { id: 'body', label: 'BODY' }
+    ]);
+
+    const edges: Edge[] = [
+      { from: 'program', to: 'p-identifier' },
+      { from: 'p-identifier', to: 'body' }
+    ];
+
+    const data: Data = { nodes, edges };
+
+    this.network = new Network(this.graphDiv.nativeElement, data, options);
+
+    this.animationSubscription = interval(0, animationFrameScheduler).pipe(
+      map(() => animationFrameScheduler.now()),
+      map(getSpinnerImageOnTime)
+    ).subscribe(svg => {
+      nodes.forEach(item => {
+        (item as any).image = svg;
+        nodes.update(item);
+      });
+    });
   }
+
+  ngOnDestroy() {
+    this.animationSubscription.unsubscribe();
+  }
+
 }
---
+++ src/app/spinner.ts
@@ -0,0 +1,30 @@
+export const images = [];
+
+for (let initialOpacity = 10; initialOpacity >= 0; initialOpacity--) {
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('width', '100');
+  svg.setAttribute('height', '100');
+  svg.setAttribute('preserveAspectRatio', 'xMidYMid');
+  for (let degree = 0; degree < 10; degree++) {
+    const opacity = (initialOpacity + degree) % 10;
+    const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    const tx = svg.createSVGTransform();
+    tx.setRotate(degree * 36, 50, 50);
+    g.transform.baseVal.appendItem(tx);
+    const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    rect.setAttribute('opacity', '' + (opacity / 10));
+    rect.x.baseVal.value = 47;
+    rect.y.baseVal.value = 27;
+    rect.rx.baseVal.value = 3;
+    rect.ry.baseVal.value = 6;
+    rect.width.baseVal.value = 6;
+    rect.height.baseVal.value = 12;
+    rect.setAttribute('fill', '#3f51b5');
+    g.appendChild(rect);
+    svg.appendChild(g);
+  }
+  images.push(URL.createObjectURL(new Blob([new XMLSerializer().serializeToString(svg)], { type: 'image/svg+xml' })));
+}
+export function getSpinnerImageOnTime(miliseconds: number) {
+  return images[Math.round(miliseconds / 100) % 10];
+}
